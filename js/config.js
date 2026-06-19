f7e7161c02d7a942bfb7df27bee7fb9d4f3881df
Yasemin Aligabi <yaseminoldac@gmail.com>
2026-06-19 15:47:12 -0400

    updated config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-1_XL6eQaNry', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5adrgikgh44s8nkmj5gm0o3poo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
